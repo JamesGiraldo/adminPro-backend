3ad091baaf932af049be609b44f9ecf580d86d5f
James Giraldo <jamesgiiraldo@gmail.com>
2021-04-10 00:10:47 -0500

    fin sesión 12

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,17 +13,20 @@ const login = async(req, res = response) => {
     try {
         /** verificar Email */
         /**  consultar los campos que existan en la base de datos */
-        const ususarioDB = await Usuario.findOne({ email });
+        const usuarioDB = await Usuario.findOne({ email });
+
         /** valdiar si el email no existe */
-        if (!ususarioDB) {
+        if (!usuarioDB) {
             /** responder el estado y mensaje formato json */
             return res.status(404).json({
                 ok: false,
                 msg: 'Contraseña o Email incorrecto.'
             });
         }
+
         /** Verificar Contraseña encriptada. */
-        const validPassword = bcrypt.compareSync(password, ususarioDB.password);
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+        
         /** valdiar si el password no existe */
         if (!validPassword) {
             /** responder el estado y mensaje formato json */
@@ -34,11 +37,12 @@ const login = async(req, res = response) => {
         }
 
         /** Generar El TOKEN JWT */
-        const token = await generarJWT(ususarioDB.id);
+        const token = await generarJWT(usuarioDB.id);
 
+        /** respuesta ok */
         res.json({
             ok: true,
-            msg: `Bienvenido ${ ususarioDB.email }`,
+            msg: `Bienvenido ${ usuarioDB.email }`,
             token: token
         });
 
@@ -59,13 +63,40 @@ const googleSignIn = async(req, res = response) => {
 
     try {
         const { name, email, picture } = await googleVerify(googleToken);
+
+        /** Validar si el usuario existe */
+        const usuarioDB = await Usuario.findOne({ email });
+
+        /** variable vacia */
+        let usuario;
+
+        if ( !usuarioDB ){
+            /** Si el usuario no existe va crear uno nuevo */
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: `@@@`,
+                img: picture,
+                google: true 
+            });
+        } else {
+            /** Si existe el usuario */
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        /** Guardar en la base de datos */
+        await usuario.save();
+
+        /** Generar El TOKEN JWT */
+        const token = await generarJWT( usuario.id );
+
+        /** respuesta ok */
         res.json({
             ok: true,
-            msg: 'Bienvanido :D con google',
-            name,
-            email,
-            picture
+            token: token
         });
+
         /**  Si lapetición esta mal mostrar el error. */
     } catch (error) {
         res.status(401).json({
