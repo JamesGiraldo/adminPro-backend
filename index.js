e30cedf64857c40c661a6f9c61750acfd34a5b38
James Giraldo <jamesgiiraldo@gmail.com>
2021-06-01 02:06:30 -0500

    excleente backend :D

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
+const path = require('path');
+
 const express = require('express');
 const cors = require('cors');
-const colors = require('colors');
+
 
 /** conección(configuración) de la base de datos */
 const { dbContection } = require('./database/config');
@@ -29,6 +31,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 app.use('/api/login', require('./routes/auth'));
+
+/** lo ultimo que si la ruta no corresponde a los path, redireccionar al index.html que es front como tal.  */
+app.get('*', ( req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 
 /** ejecutar servidor en el puerto que esta las variables de entorno */
 app.listen(process.env.PORT, () => {
